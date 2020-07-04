75324fe37a95b8ee9b4276366dd4011fb8e7a225
Christopher Dutcher <cadutcher@icloud.com>
2020-07-04 00:54:21 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(dog_name)
+    @new_dog_name = dog_name
+  end
+
+  def name
+    @new_dog_name
+  end
+
+  def breed=(dog_breed)
+    @new_dog_breed = dog_breed
+  end
+
+  def breed
+    @new_dog_breed
+  end
+end 
